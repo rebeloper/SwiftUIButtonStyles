6fd9b4651bfc99592c861e5aa24a4a4cbb8f2b45
Alex Nagy <rebeloper@users.noreply.github.com>
2021-02-22 10:50:04 +0200

    Initial Commit

---
+++ Shared/SwiftUIButtonStylesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIButtonStylesApp.swift
+//  Shared
+//
+//  Created by Alex Nagy on 22.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIButtonStylesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
